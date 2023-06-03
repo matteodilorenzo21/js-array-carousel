438c2ef1b8f8feafe7edc509ae791efab35a0f0b
matteodilorenzo21 <matteodilorenzo21@gmail.com>
2023-06-03 10:21:04 +0200

    added reactive thumbnail bonus

--- JS/main.js
+++ JS/main.js
@@ -9,12 +9,15 @@ const nextBtn = document.getElementById('next');
 // Recuperiamo le immagini del Carousel
 const images = document.querySelectorAll('#img-carousel img');
 
+const thumbnails = document.querySelectorAll('#img-thumbnail img')
+
 // Creo la variabile contenente l'index corrente di "images"
 let currentIndex = 0;
 
 // Settiamo la prima img come attiva
 images[currentIndex].classList.add('active');
 
+thumbnails[currentIndex].classList.add('show');
 
 // Metto il bottone NEXT in ascolto
 nextBtn.addEventListener('click', function() {
@@ -22,14 +25,18 @@ nextBtn.addEventListener('click', function() {
     // rimuovo la classe 'active' al currenIndex
     images[currentIndex].classList.remove('active');
 
+    thumbnails[currentIndex].classList.remove('show');
+
     // incremento il currentIndex
-    images[currentIndex++];
+    currentIndex++;
     if(currentIndex >= images.length) {
         currentIndex = 0;
     }
     
     // aggiungo la classe 'active' al currentIndex successivo
     images[currentIndex].classList.add('active');
+
+    thumbnails[currentIndex].classList.add('show');
     
 })
 
@@ -41,15 +48,19 @@ prevBtn.addEventListener('click', function() {
     
     // rimuovo la classe 'active' al currenIndex
     images[currentIndex].classList.remove('active')
+
+    thumbnails[currentIndex].classList.remove('show');
     
     // incremento il currentIndex
-    images[currentIndex--];
+    currentIndex--;
     if(currentIndex < 0) {
         currentIndex = images.length - 1;
     }
     
     // aggiungo la classe 'active' al currentIndex successivo
     images[currentIndex].classList.add('active');
+
+    thumbnails[currentIndex].classList.add('show');
                 
 })
 
